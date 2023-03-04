3cb7e54c7926fced7dfc97d50d058eae4fdcaf96
p1ass <me@p1ass.com>
2023-03-04 17:58:21 +0900

    iconをtree shaking

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,18 +1,38 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
+import { FaArrowLeft, FaArrowRight, FaCheckCircle, FaPlay, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
 type Props = {
-  type: keyof typeof Icons;
+  type: 'FaUser' | 'FaShoppingCart' | 'FaArrowLeft' | 'FaArrowRight' | 'FaPlay' | 'FaCheckCircle';
   width: number;
   height: number;
   color: string;
 };
 
 export const Icon: FC<Props> = ({ color, height, type, width }) => {
-  const Icon = Icons[type];
+  let Icon;
+  switch (type) {
+    case 'FaUser':
+      Icon = FaUser;
+      break;
+    case 'FaShoppingCart':
+      Icon = FaShoppingCart;
+      break;
+    case 'FaArrowLeft':
+      Icon = FaArrowLeft;
+      break;
+    case 'FaArrowRight':
+      Icon = FaArrowRight;
+      break;
+    case 'FaPlay':
+      Icon = FaPlay;
+      break;
+    case 'FaCheckCircle':
+      Icon = FaCheckCircle;
+      break;
+  }
   return (
     <span className={classNames(type, styles.container({ color, height, width }))}>
       <Icon />
