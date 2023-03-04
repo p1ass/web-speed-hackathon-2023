69f84082c5ed606bc1a1e5d3910fa747c9248263
p1ass <me@p1ass.com>
2023-03-04 23:57:16 +0900

    これは入れる

--- src/server/graphql/review_resolver.ts
+++ src/server/graphql/review_resolver.ts
@@ -1,12 +1,27 @@
-import type { Review } from '../../model/review';
+import { Review } from '../../model/review';
+import { dataSource } from '../data_source';
 
 import type { GraphQLModelResolver } from './model_resolver';
 
 export const reviewResolver: GraphQLModelResolver<Review> = {
   product: async (parent) => {
-    return parent.product;
+    const review = await dataSource.manager.findOneOrFail(Review, {
+      relations: {
+        product: true,
+      },
+      where: { id: parent.id },
+    });
+
+    return review.product;
   },
   user: async (parent) => {
-    return parent.user;
+    const review = await dataSource.manager.findOneOrFail(Review, {
+      relations: {
+        user: true,
+      },
+      where: { id: parent.id },
+    });
+
+    return review.user;
   },
 };
