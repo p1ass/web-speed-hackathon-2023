26a5c335d3423832e2b31c1a1a2749ebcb512356
p1ass <me@p1ass.com>
2023-03-04 15:09:53 +0900

    set cache control

--- src/server/index.ts
+++ src/server/index.ts
@@ -31,7 +31,7 @@ async function init(): Promise<void> {
   app.use(session({}, app));
 
   app.use(async (ctx, next) => {
-    ctx.set('Cache-Control', 'no-store');
+    ctx.set('Cache-Control', 'max-age=604800');
     await next();
   });
 
